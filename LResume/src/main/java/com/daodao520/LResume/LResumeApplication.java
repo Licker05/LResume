1b0d4b24dfe30394ac9145dc3c85a1e660a8f3a4
Licker <460319984@qq.com>
2018-06-12 15:03:58 +0800

    初始化
    初始化

---
+++ LResume/src/main/java/com/daodao520/LResume/LResumeApplication.java
@@ -0,0 +1,12 @@
+package com.daodao520.LResume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LResumeApplication.class, args);
+	}
+}
---
+++ LResume/src/test/java/com/daodao520/LResume/LResumeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daodao520.LResume;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LResumeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
