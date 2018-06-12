cce88d5e5153ffc9c7705037c64d7f7cb9be3abd
your name <460319984@qq.com>
2018-06-12 22:09:21 +0800

    初始化控制器

---
+++ LResume/src/main/java/com/nowcoder/controller/homeController.java
@@ -0,0 +1,17 @@
+package com.nowcoder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class homeController {
+    @RequestMapping(path={"/"},method = {RequestMethod.GET,RequestMethod.POST})
+    public String home(){
+        return "home";
+    }
+    @RequestMapping(path={"/login"},method = {RequestMethod.GET,RequestMethod.POST})
+    public String login(){
+        return "login";
+    }
+}
