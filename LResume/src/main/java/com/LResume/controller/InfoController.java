18db12b844937613b964d196e8e3b687fd1265c4
your name <460319984@qq.com>
2018-06-21 19:56:45 +0800

    完善了上传头像和查看头像

--- LResume/src/main/java/com/LResume/controller/InfoController.java
+++ LResume/src/main/java/com/LResume/controller/InfoController.java
@@ -2,19 +2,26 @@ package com.LResume.controller;
 
 import com.LResume.model.User;
 import com.LResume.model.UserInfo;
+import com.LResume.service.QiniuService;
 import com.LResume.service.UserInfoService;
 import com.LResume.utils.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 
 @Controller
 public class InfoController {
     @Autowired
     UserInfoService userInfoService;
+    @Autowired
+    QiniuService qiniuService;
     @RequestMapping(path = {"/user/info/{userId}"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String index(Model model, @PathVariable("userId") int userId) {
         return "info";
@@ -30,17 +37,36 @@ public class InfoController {
                          @RequestParam("email") String email,
                          @RequestParam("remark") String remark,
                          HttpServletResponse res){
-//        System.out.println("username = "+username);
-//        System.out.println("nickname = "+nickname);
-//        System.out.println("sex = "+sex);
-//        System.out.println("headurl = "+headurl);
-//        System.out.println("cellphone = "+cellphone);
-//        System.out.println("email = "+email);
-//        System.out.println("remark = "+remark);
         userInfoService.updateUser(username,headurl,cellphone,email);
         userInfoService.updateUserInfo(username,nickname,sex,remark);
         return JSONUtil.getJSONString(0, "成功");
     }
+    @RequestMapping(path = {"/image"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public void getImage(@RequestParam("name") String imageName,
+                         HttpServletResponse response) {
+        try {
+            response.setContentType("image/jpeg");
+            StreamUtils.copy(new FileInputStream(new
+                    File(JSONUtil.IMAGE_DIR + imageName)), response.getOutputStream());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    @RequestMapping(path = {"/uploadImage/"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public String uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileUrl = qiniuService.saveImage(file);
+            if (fileUrl == null) {
+                return JSONUtil.getJSONString(1, "上传图片失败");
+            }
+            System.out.println(fileUrl);
+            return JSONUtil.getJSONString(0, fileUrl);
+        } catch (Exception e) {
+            return JSONUtil.getJSONString(1, "上传失败");
+        }
+    }
 }
 
---
+++ LResume/src/main/java/com/LResume/service/QiniuService.java
@@ -0,0 +1,71 @@
+package com.LResume.service;
+
+import com.LResume.utils.JSONUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.qiniu.common.QiniuException;
+import com.qiniu.common.Zone;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Created by licker
+ */
+@Service
+public class QiniuService {
+    private static final Logger logger = LoggerFactory.getLogger(QiniuService.class);
+    //设置好账号的ACCESS_KEY和SECRET_KEY
+    String ACCESS_KEY = "cENIbHWzKYT6VnUAyT4svoQ5BHzNeao5CU-QAYS-";
+    String SECRET_KEY = "-5aLfH-Regjau_A6BWGd6EbM1iI4t88d16csLkgb";
+    //要上传的空间
+    String bucketname = "daodao520";
+
+    //密钥配置
+    Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+    //创建上传对象
+    Configuration cfg = new Configuration(Zone.zone0());
+    UploadManager uploadManager = new UploadManager(cfg);
+
+    private static String QINIU_IMAGE_DOMAIN = "http://p8wefufch.bkt.clouddn.com/";
+
+    //简单上传，使用默认策略，只需要设置上传的空间名就可以了
+    public String getUpToken() {
+        return auth.uploadToken(bucketname);
+    }
+
+    public String saveImage(MultipartFile file) throws IOException {
+        try {
+            int dotPos = file.getOriginalFilename().lastIndexOf(".");
+            if (dotPos < 0) {
+                return null;
+            }
+            String fileExt = file.getOriginalFilename().substring(dotPos + 1).toLowerCase();
+            if (!JSONUtil.isFileAllowed(fileExt)) {
+                return null;
+            }
+
+            String fileName = UUID.randomUUID().toString().replaceAll("-", "") + "." + fileExt;
+            //调用put方法上传
+            Response res = uploadManager.put(file.getBytes(), fileName, getUpToken());
+            //打印返回的信息
+            if (res.isOK() && res.isJson()) {
+                return QINIU_IMAGE_DOMAIN + JSONObject.parseObject(res.bodyString()).get("key");
+            } else {
+                logger.error("七牛异常:" + res.bodyString());
+                return null;
+            }
+        } catch (QiniuException e) {
+            // 请求失败时打印的异常的信息
+            logger.error("七牛异常:" + e.getMessage());
+            return null;
+        }
+    }
+}
