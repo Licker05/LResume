faff2eb2cc8c0b18672736b0262d92ada0c2738e
your name <460319984@qq.com>
2018-06-22 09:56:30 +0800

    更新首页

--- LResume/src/main/java/com/LResume/configuration/ToutiaoWebConfiguration.java
+++ LResume/src/main/java/com/LResume/configuration/ToutiaoWebConfiguration.java
@@ -23,7 +23,7 @@ public class ToutiaoWebConfiguration extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(passportInterceptor);
         registry.addInterceptor(loginRequiredInterceptor).
-                addPathPatterns("/user/info/").addPathPatterns("/user/info/");
+                addPathPatterns("/user/info/").addPathPatterns("/user/info/*");
         super.addInterceptors(registry);
     }
 }
--- LResume/src/main/java/com/LResume/controller/EditController.java
+++ LResume/src/main/java/com/LResume/controller/EditController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ErrorController {
-    @RequestMapping(path = {"/error"},method = {RequestMethod.GET,RequestMethod.POST})
-    public String error(){
-        return "error";
+public class EditController {
+    @RequestMapping(path = {"/form"},method = {RequestMethod.GET,RequestMethod.POST})
+    public String form(){
+        return "form";
     }
-
 }
