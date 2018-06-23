9632262beeab99a873eeff832bdc4e9d1ff2209e
your name <460319984@qq.com>
2018-06-23 16:50:54 +0800

    增加了上传

--- LResume/src/main/java/com/LResume/controller/EditController.java
+++ LResume/src/main/java/com/LResume/controller/EditController.java
@@ -2,10 +2,7 @@ package com.LResume.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,7 +19,10 @@ public class EditController {
                              HttpServletRequest request,
                              HttpServletResponse response){
         System.out.println(request.getParameter("politics"));
-        System.out.println(request.getParameter("desc"));
+//        System.out.println(politics);
+//        for(String job:jobstarttime){
+//            System.out.println(job);
+//        }
         return "123";
     }
 }
